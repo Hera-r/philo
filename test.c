27efc9a0d974cff04eb00824fc81d4922062f61a
Hera.r <tennyhera.r@gmail.com>
2023-10-27 21:09:39 +0200

    save

--- test.c
+++ test.c
@@ -1,14 +1,57 @@
 #include <stdio.h>
 #include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+	struct s_data	*data;
+	pthread_t		thread;
+	int				id;
+	int				eat_cont;
+	int				status;
+	int				eating;
+	int				time_to_die;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	*r_fork;
+	pthread_mutex_t	*l_fork;
+}					t_philo;
 
 typedef struct s_data
 {
-	int				id;
+	pthread_t		*tid;
+	t_philo			*thread;
 	int				nb_philo;
-	pthread_t		*philospher;
+	int				meals_nb;
+	int				dead;
+	int				finished;
+	int				death_time;
+	long int		eat_time;
+	int				sleep_time;
+	long int		start_time;
 	pthread_mutex_t	*forks;
+	pthread_mutex_t	lock;
+	pthread_mutex_t	write;
+}					t_data;
 
-}				t_pihlo;
+long int	current_time(void)
+{
+	struct timeval	tv;
+
+	if (gettimeofday(&tv, NULL) == -1)
+		return (printf("ERR_TIME\n"), 1);
+	return ((tv.tv_sec * 1000) + (tv.tv_usec / 1000));
+}
+
+int	ft_usleep(long int time)
+{
+	long int	start;
+	start = current_time();
+	while ((current_time() - start) < time)
+		usleep(time / 10);
+	return(0);
+}
 
 static int	is_space(char c)
 {
@@ -17,14 +60,12 @@ static int	is_space(char c)
 	return (0);
 }
 
-
 int	isdigit(int c)
 {
 	if (c >= 48 && c <= 57)
 		return (1);
 	return (0);
 }
-
 
 int	xatoi(const char *nptr)
 {
@@ -51,8 +92,7 @@ int	xatoi(const char *nptr)
 	return (n * sign);
 }
 
-
-int all_digit(char *argv[])
+int	all_digit(char *argv[])
 {
 	int	i;
 	int	j;
@@ -73,52 +113,170 @@ int all_digit(char *argv[])
 	return (0);
 }
 
-
-
-
-void	*philosopher(void *init)
+void	destroy_mutex(pthread_mutex_t *data, int nb_philo)
 {
-	t_data	*data;
-	int		id;
+	int	i;
 
-	
-	data = (t_data*)init;
-	left_fork = id;
-	right_fork = (id + 1) % data.nb_philo;
-	while (1)
+	i = 0;
+	while (i < nb_philo)
 	{
-
+		pthread_mutex_destroy(&data[i]);
+		i++;
 	}
-	
-
 }
 
-
-int main(int argc, char *argv[])
+int	init_mutex(t_data *data)
 {
-	t_data	data;
-	int nb_philo;
+	int	i;
 
-	if (argc < 5 || argc > 6)
+	i = 0;
+	data->forks = malloc(data->nb_philo * sizeof(pthread_mutex_t));
+	if(!data->forks)
 		return (0);
-	if (all_digit(argv) == 1)
-		return (0);
-	nb_philo = xatoi(argv[1]);
-	if (nb_philo < 2)
-		return (0);
-	data = init_philo(nb_philo);
-
-
-
-
+	while (i < data->nb_philo)
+	{
+		if (pthread_mutex_init(&data->forks[i], NULL) != 0)
+			return (destroy_mutex(data->forks, data->nb_philo), 1);
+		i++;
+	}
 	return (0);
 }
 
+int	take_forks(t_philo *philo)
+{
+	long int	xtime;
+
+	if (pthread_mutex_lock(&philo->l_fork) != 0)
+		return (printf("Error pthread_mutex_lock\n", 1));
+	xtime = current_time() - philo->data->start_time;
+	printf("%ld %d has taken a fork\n", xtime, philo->id);
+	if (pthread_mutex_lock(&philo->r_fork) != 0)
+		return (printf("Error pthread_mutex_lock\n", 1));
+	xtime = current_time() - philo->data->start_time;
+	printf("%ld %d has taken a fork\n", xtime, philo->id);
+	return (0);
+}
+
+void	xeating(t_philo *philo)
+{
+	long int	xtime;
+
+	xtime = current_time() - philo->data->start_time;
+	printf("%ld %d is eating\n", xtime, philo->id);
+	ft_usleep(philo->eating);
+}
+
+int	sleeping(t_philo *philo)
+{
+	long int	xtime;
+
+	if (pthread_mutex_unlock(&philo->l_fork) != 0)
+		return (printf("Error pthread_mutex_unlock\n", 1));
+	if (pthread_mutex_unlock(&philo->r_fork) != 0)
+		return (printf("Error pthread_mutex_unlock\n", 1));
+	xtime = current_time() - philo->data->start_time;
+	printf("%ld %d is sleeping\n", xtime, philo->id);
+	ft_usleep(philo->eating);
+	return (0);
+}
+
+void	thinking(t_philo *philo)
+{
+	long int	xtime;
+
+	xtime = current_time() - philo->data->start_time;
+	printf("%ld %d is thinking\n", xtime, philo->id);
+	ft_usleep(philo->eating);
+}
 
 
+t_philo	*init_xphilo(t_data *data)
+{
+	t_philo	*philo;
+	int		i;
+	int		tmp;
 
+	philo = malloc((data->nb_philo) + sizeof(t_philo));
+	if (!philo)
+		return (NULL);
+	memset(philo, 0, sizeof(t_philo) * data->nb_philo);
+	i = 0;
+	while (i < data->nb_philo)
+	{
+		philo[i].id = i;
+		philo[i].l_fork = &data->forks[philo[i].id];
+		tmp = (philo[i].id + 1) % data->nb_philo;
+		philo[i].r_fork = &data->forks[tmp];
+		philo[i].eating = &data->eat_time;
+		philo[i].data = data;
+	}
+	return (philo);
+}
 
+int	init_argv(t_data *data, int argc, char *argv[])
+{
+	if (argc == 6)
+		data->meals_nb = xatoi(argv[5]);
+	else
+		data->meals_nb = 0;
+	data->nb_philo = xatoi(argv[1]);
+	data->death_time = xatoi(argv[2]);
+	data->eat_time = xatoi(argv[3]);
+	data->sleep_time = xatoi(argv[4]);
+	if (data->nb_philo <= 1 || data->death_time < 0 || 
+		data->eat_time < 0 || data->sleep_time || data->meals_nb < 0)
+		return (1);
+	return (0);
+}
 
+int	xparsing(t_data *data, int argc, char *argv[])
+{
+	if (all_digit(argv) == 1)
+	{
+		printf("Is not digit\n");
+		return (1);
+	}
+	if (init_argv(data, argc, argv) == 1)
+		return (0);
+	return (1);
+}
+
+void	*xroutine(void *arg)
+{
+	t_philo	*philo;
+
+	philo = (t_philo *)arg;
+	while (42)
+	{
+		take_forks(philo);
+		xeating(philo);
+		sleeping(philo);
+		thinking(philo);
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	t_data		data;
+	pthread_t	*philo;
+	int			i;
+
+	if (argc < 5 || argc > 6)
+	{
+		printf("error 1\n");
+		return (0);
+	}
+	if (xparsing(&data, argc, argv) == 1)
+		return (0);
+
+	// if (init_mutex(&data) == 1)
+	// 	return (0);
+	// i = 0;
+	// philo = init_xphilo(&data);
+	data.start_time = current_time();
+
+	return (0);
+}
 
 
 
@@ -127,79 +285,10 @@ int main(int argc, char *argv[])
 
 
 
-// pthread_mutex_t	*inti_forks(int nb_philo)
-// {
-// 	pthread_mutex_t	*forks;
-// 	int				i;
-
-// 	i = 1;
-// 	forks = malloc(nb_philo * sizeof(pthread_mutex_t));
-// 	if (!forks)
-// 		return ;
-// 	while (i <= nb_philo)
-// 	{
-// 		if (pthread_mutex_init(&forks[i], NULL) != 0)
-// 			return (free(forks), NULL);
-// 		i++;
-// 	}
-// 	return (forks);
-// }
-
-// t_data	init_philo(int nb_philo)
-// {
-// 	t_data	data;
-
-// 	data.nb_philo = nb_philo;
-// 	data.philospher = tab_pihlo(nb_philo);
-// 	data.forks = inti_forks(nb_philo);
-
-// 	return (data);
-// }
 
 
 
 
-
-
-
-	// philo = malloc(nb_philo * sizeof(pthread_t));
-	// if (!philo)
-	// 	return ;
-
-
-
-// // Définir la fonction que chaque "lutin" (thread) va exécuter
-// void* travailDuLutin(void* numero) {
-//     int num = *(int*)numero;
-	
-//     if (num == 1) {
-//         printf("Le lutin 1 dessine un soleil.\n");
-//     } else if (num == 2) {
-//         printf("Le lutin 2 écrit une lettre.\n");
-//     }
-
-//     return NULL;
-// }
-
-// int main() {
-//     // Préparer les "lutins"
-//     pthread_t lutin1, lutin2;
-//     int num1 = 1, num2 = 2;
-
-//     // Dire aux "lutins" ce qu'ils vont faire
-
-
-//     pthread_create(&lutin1, NULL, travailDuLutin, (void*)&num1);
-
-
-//     pthread_create(&lutin2, NULL, travailDuLutin, (void*)&num2);
-
-//     // Attendre que les "lutins" finissent leur travail
-//     pthread_join(lutin1, NULL);
-//     pthread_join(lutin2, NULL);
-
-//     return 0;
-// }
 
 
 
