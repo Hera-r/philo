dd72c96fa8f9fed98a8fd10dca41da1a9fb97980
Hera-r <tennyhera.r@gmail.com>
2023-10-30 16:20:02 +0000

    tmp

--- actions.c
+++ actions.c
@@ -2,14 +2,20 @@
 
 #include "philo.h"
 
-int	everyone_ate(t_philo *philo)
-{
-	if (philo->eat_cont == philo->data->meals_nb)
-		philo->data->finished += philo->data->meals_nb;
-	if (philo->data->finished == (philo->data->meals_nb * philo->data->meals_nb))
-		return (1);
-	return (0);
-}
+// int	everyone_ate(t_philo *philo)
+// {
+// 	if (philo->eat_cont == philo->data->meals_nb)
+// 	{
+// 		if (pthread_mutex_lock(&philo->data->finished) != 0)
+// 			return (printf("pthread_mutex_lock"), 1);
+// 		philo->data->finished += philo->data->meals_nb;
+// 	}
+// 	if (pthread_mutex_lock(&philo->data->finished) != 0)
+// 		return (printf("pthread_mutex_lock\n"), 1);
+// 	if (philo->data->finished == (philo->data->meals_nb * philo->data->meals_nb))
+// 		return (1);
+// 	return (0);
+// }
 
 
 int	take_forks(t_philo *philo)
@@ -17,13 +23,13 @@ int	take_forks(t_philo *philo)
 	long int	xtime;
 
 	if (pthread_mutex_lock(philo->l_fork) != 0)
-		return (perror("pthread_mutex_lock"), 1);
+		return (printf("pthread_mutex_lock"), 1);
 	
 	xtime = current_time() - philo->data->start_time;
 	printf("%ld %d has taken a fork\n", xtime, philo->id + 1);
 
 	if (pthread_mutex_lock(philo->r_fork) != 0)
-		return (perror("pthread_mutex_lock"), 1);
+		return (printf("pthread_mutex_lock"), 1);
 
 	xtime = current_time() - philo->data->start_time;
 	printf("%ld %d has taken a fork\n", xtime, philo->id + 1);
@@ -57,3 +63,13 @@ int	sleeping(t_philo *philo)
 	ft_usleep(philo->data->sleep_time);
 	return (0);
 }
+
+
+// int	check_philoshopher_dead(t_philo *philo)
+// {
+// 	long int	current;
+// 	long int	elapsed;
+
+// 	current = current_time();
+// 	elapsed = current - l
+// }
